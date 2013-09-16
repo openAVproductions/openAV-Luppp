f46b85b4bc66aa6ee4d08558ef942de049a0339a
Harry van Haaren <harryhaaren@gmail.com>
2013-09-16 19:52:09 +0100

    -Removed Tup leftovers

--- fixTup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# sometimes Tup gives errors on FUSE mount or something
-# just deleting .tup/mnt/* makes tup re-mount things, fixing it:
-
-rm -rf .tup/mnt/*
-rm -rf build/src/*
-rm -rf buildTest/src/*
