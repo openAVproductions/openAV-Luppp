3b842c0d08b9a4d3934154c0bdda4b8a891c1658
Harry van Haaren <harryhaaren@gmail.com>
2013-09-18 13:55:15 +0100

    -#ifdef-ed Jack print

--- src/jack.cxx
+++ src/jack.cxx
@@ -357,7 +357,9 @@ void Jack::midiObserverWriteMIDI( int portIndex, unsigned char* data )
   if( buf != 0 )
   {
     memcpy( buf, data, sizeof( unsigned char ) * 3);
+#ifdef DEBUG_MIDI
     cout << "midiObserverWriteMIDI portID " << portIndex << "  " << int(buf[0]) << ", " << int(buf[1]) << ", " << int(buf[2]) << endl;
+#endif
   }
   else
   {
