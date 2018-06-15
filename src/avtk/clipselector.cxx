ac1ab3edf478043d0d99b518dc6ec76148e76ed2
Georg Krause <mail@georg-krause.net>
2018-06-15 21:11:17 +0200

    hotfix, mouse interaction possible again

--- src/avtk/clipselector.cxx
+++ src/avtk/clipselector.cxx
@@ -338,8 +338,6 @@ int ClipSelector::handle(int event)
 			if (clipNum >= numClips)
 				clipNum = numClips -1; // fix for clicking the lowest pixel
 
-			return clipNum;
-
 			// handle right clicks: popup menu
 			if ( Fl::event_state(FL_BUTTON3) ) {
 				if ( _master ) {
